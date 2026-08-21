c1bd5acaa8d10ebdebeac35bb802f814776b23b7
Aansh kumar singh <anshsingh1147@gmail.com>
2026-08-21 22:48:08 +0530

    ready0.1

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,23 @@ import "dotenv/config";
 
 import {app } from "./app.js";
 
+import cors from 'cors';
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://naari-final-git-main-artansscript.vercel.app/'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
+
 import connectDB  from "./config/db.js"
 connectDB()
 const PORT = process.env.PORT || 8000
