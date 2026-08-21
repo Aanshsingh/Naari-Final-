328e8fb39f78293e77dd2c32178be95b9394572b
Aanshsingh <anshsingh1147@gmail.com>
2026-08-21 23:18:06 +0530

    Remove CORS setup from index.js
    Removed CORS middleware and allowed origins configuration.

--- Server/index.js
+++ Server/index.js
@@ -2,22 +2,7 @@ import "dotenv/config";
 
 import {app } from "./app.js";
 
-import cors from 'cors';
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'naari-final.vercel.app'
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
 
 import connectDB  from "./config/db.js"
 connectDB()
