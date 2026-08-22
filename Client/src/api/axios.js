595e397e3979d92004f653da3f76cdbe1a0ca4d2
Aanshsingh <anshsingh1147@gmail.com>
2026-08-22 07:22:59 +0530

    Update API base URL in axios configuration

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://naari-final-3.onrender.com/api/v1',
   withCredentials: true,
 });
 
